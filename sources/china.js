21eac08ee73a5b667604a3a4aa447a5eeeec90de
Camille <lecamille@gmail.com>
2022-08-03 23:10:54 -0700

    Add Hong Kong

---
+++ sources/china.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    country: "China",
+    city: "Hong Kong",
+    short: "Hong Kong",
+    long: "Hong Kong",
+    id: "hong_kong",
+    id_city_name: "hong_kong",
+    primary: "hong_kong",
+    center: null,
+    latitude: null,
+    longitude: null,
+    info: "https://hub.arcgis.com/datasets/esrihk::old-and-valuable-trees-and-stonewall-trees-in-hong-kong/about",
+    srs: null,
+    brokenDownload: false,
+    download:
+      "https://opendata.arcgis.com/api/v3/datasets/1eb1ece1b6264d5abf85feb067e33508_0/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1",
+    format: "geojson",
+    filename: "Old_and_Valuable_Trees_and_Stonewall_Trees_in_Hong_Kong.geojson",
+    gdal_options: null,
+    license: null,
+    email: null,
+    contact: null,
+    crosswalk: {
+      species: "Species",
+      notes: "Mitigation Measure",
+      health: "Condition",
+      location: "Location",
+      updated: "Last Inspection Date",
+    },
+  },
+];
+
