28b7502f70f7ea777bf3f32b2ffa3f64041bcbc2
Tom Z <tzinckgraf@gmail.com>
2023-02-20 15:46:36 -0500

    feature/tree-sources-staging
    Added a new merge step to the ETL process. The new merge step takes all
    the data from the geojson file and moves it into a staging table called
    `tree_sources_staging`. This is a way to keep the raw data from the
    sources into a database table.
    
    This PR uses ogr2ogr to move data into the database. By default, data is
    moved 20k rows at a time. Postgres is all lowercase, and javascript uses
    camelcase, so we cannot easily use underscores in names without some
    bigger changes. However, that did not seem to be an issue.
    
    feature/tree-sources-staging
    Removed unused new library

---
+++ src/stages/merge.js
@@ -0,0 +1,66 @@
+import { db } from '../db/index.js';
+import { exec } from 'child_process';
+import { dbConfig } from '../db/db-config.js';
+import pLimit from "p-limit";
+import * as utils from "../core/utils.js";
+
+export const mergeSource = async (source) => {
+  console.log('source.id', source.id);
+  if (
+    !source.destinations ||
+    !source.destinations.geojson ||
+    !source.destinations.normalized
+  ) {
+    throw new Error(`No destinations for source: "${source}"`);
+  }
+
+  const normalizedExists = await utils.asyncFileExists(
+    source.destinations.normalized.path
+  );
+  if (!normalizedExists) {
+    console.log(
+      `The normalized file '${source.destinations.normalized.path}' does not exists. Skipping...`
+    );
+    return `NO FILE for ${source.id}`; // Early Return
+  }
+
+    console.log(`Running for ${source.destinations.normalized.path}`);
+    // FIXME use the async version of exec, but that means a new dependecrunMergey
+    const command = `ogr2ogr -f "PostgreSQL" PG:"host=${dbConfig.host} user=${dbConfig.user} password=${dbConfig.password} dbname=${dbConfig.database}" ${source.destinations.normalized.path} -nln tree_sources_staging -geomfield geom -append`
+    exec(command, async (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+  const context = {
+    source,
+    nullGeometry: 0,
+    invalidGeometry: 0,
+  };
+
+  return context;
+};
+
+
+export const mergeSources = async (list) => {
+  const limit = pLimit(5);
+  const promises = list.map((source) =>
+    limit(() => mergeSource(source))
+  );
+  const results = await Promise.allSettled(promises);
+  console.log("Finished uploading to the database...");
+  results.forEach((l) => {
+    if (l && l.forEach) {
+      l.forEach(console.log);
+    } else {
+      console.log(l);
+    }
+  });
+};
