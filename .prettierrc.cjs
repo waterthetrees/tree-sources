80349c45e8e5e9207e390677be66b4eb8461663d
zoobot <rommims@gmail.com>
2023-03-26 15:20:05 -0700

    Added .eslintrc.cjs and .prettierrc.cjs, removed .eslintrc.js

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80, // https://prettier.io/docs/en/options.html#print-width
+  tabWidth: 2, // https://prettier.io/docs/en/options.html#tab-width
+  useTabs: false, // https://prettier.io/docs/en/options.html#tabs
+  semi: true, // https://prettier.io/docs/en/options.html#semicolons
+  singleQuote: true, // https://prettier.io/docs/en/options.html#quotes
+  quoteProps: 'as-needed', // https://prettier.io/docs/en/options.html#quote-props
+  trailingComma: 'all', // https://prettier.io/docs/en/options.html#trailing-commas
+  bracketSpacing: true, // https://prettier.io/docs/en/options.html#bracket-spacing
+  arrowParens: 'always', // https://prettier.io/docs/en/options.html#arrow-function-parentheses
+};
