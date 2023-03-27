54bc17aebd8d6964af4877e10900ed9217aff576
Tom Z <tzinckgraf@gmail.com>
2023-03-26 21:13:15 -0400

    feature/tree-sources-staging - update the ogr2ogr command to transform the sql converting camelCase to snake_case - convert database table to treedata_staging - use spawn instead of exec - truncate the staging table as the first step
    Added a new line for a little clean up

--- src/stages/merge.js
+++ src/stages/merge.js
@@ -1,11 +1,12 @@
-import { db } from '../db/index.js';
-import { exec } from 'child_process';
-import { dbConfig } from '../db/db-config.js';
+import { spawn } from "child_process";
+import { dbConfig } from "../db/db-config.js";
+import { db } from "../db/index.js";
 import pLimit from "p-limit";
 import * as utils from "../core/utils.js";
 
+
 export const mergeSource = async (source) => {
-  console.log('source.id', source.id);
+  console.log(`Starting merge for ${source.idName}`);
   if (
     !source.destinations ||
     !source.destinations.geojson ||
@@ -19,41 +20,64 @@ export const mergeSource = async (source) => {
   );
   if (!normalizedExists) {
     console.log(
-      `The normalized file '${source.destinations.normalized.path}' does not exists. Skipping...`
+      `The normalized file "${source.destinations.normalized.path}" does not exists. Skipping...`
     );
-    return `NO FILE for ${source.id}`; // Early Return
+    return `NO FILE for ${source.idName}`; // Early Return
   }
 
-    console.log(`Running for ${source.destinations.normalized.path}`);
-    // FIXME use the async version of exec, but that means a new dependecrunMergey
-    const postgresConfig = `host=${dbConfig.host} user=${dbConfig.user}`
-        + ` password=${dbConfig.password} dbname=${dbConfig.database}`;
-    const command = `ogr2ogr -f "PostgreSQL" PG:"${postgresConfig}"`
-        + ` ${source.destinations.normalized.path} -nln tree_sources_staging`
-        + ' -geomfield geom -append -update --config PG_USE_COPY YES';
-    exec(command, async (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+  console.log(`Running for ${source.destinations.normalized.path}`);
+  /*
+    * For debugging purposes, you can check the contents of the geojson file with ogr2ogr
+    * using the following command:
+    * ogrinfo data/normalized/[sourceId].geojsons
+    *  -dialect SQLite
+    *  -sql 'select idReference as id_reference, idName as id_source_name, * from "[sourceId]"'
+    */
+  const postgresConfig = `postgresql://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:5432/${dbConfig.database}`;
+  const command = "ogr2ogr";
+  const commandArgs = [
+    "-f", "PostgreSQL", `PG:${postgresConfig}`,
+    "-dialect", "SQLite",
+    "-sql",
+    `select idReference as id_reference, idName as id_source_name, * from ${source.idName}`,
+    "-nln", "treedata_staging",
+    "-geomfield", "geom",
+    source.destinations.normalized.path,
+    "-append", "-update", "--config", "PG_USE_COPY", "YES", "-preserve_fid"
+  ];
 
-  const context = {
-    source,
-    nullGeometry: 0,
-    invalidGeometry: 0,
-  };
+  return new Promise((resolve, _) => {
+    const child = spawn(
+      command,
+      commandArgs,
+      {
+        stdio: ["ignore", process.stdout, process.stderr],
+      }
+    );
+    child.on("exit", resolve);
+  });
 
-  return context;
 };
 
 
+const truncateStaging = async () => {
+  return await db.none('TRUNCATE TABLE treedata_staging');
+}
+
+
 export const mergeSources = async (list) => {
+  console.log("Start truncate the treedata_staging database...");
+  try {
+    await truncateStaging();
+  } catch(error) {
+		console.error(
+      '\nError truncating the treedata_staging database...',
+      inspect(error, true, 2, true));
+		throw error;
+  }
+  console.log("Done truncating the treedata_staging database...");
+
+  console.log("Start uploading to the database...");
   const limit = pLimit(5);
   const promises = list.map((source) =>
     limit(() => mergeSource(source))
